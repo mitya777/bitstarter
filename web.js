da34e251434da8c7ea4d897b5217fb49d18eb963
Dmitriy Fabrikant <mityaf@gmail.com>
2013-07-11 18:07:39 +0000

    read index.html from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+
+var fs = require('fs');
+var contents = fs.readFileSync('index.html');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
