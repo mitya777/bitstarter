ffdd55727c5709b7681aa623dc8a0e55759b141a
Ubuntu <ubuntu@ip-172-31-8-159.us-west-2.compute.internal>
2013-07-31 17:00:42 +0000

    placeholder bitstarter

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 
 var fs = require('fs');
-var contents = fs.readFileSync('index.html');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var contents = fs.readFileSync('index.html');
   response.send(contents.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
